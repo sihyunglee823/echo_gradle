c32b61040715b803e7b5cad5b2ba1f8b1eadb6ee
sihyunglee823 <93116885+sihyunglee823@users.noreply.github.com>
2022-02-09 17:12:47 +0900

    Update EchoController.java

--- src/main/java/com/example/demo_gradle/echo/EchoController.java
+++ src/main/java/com/example/demo_gradle/echo/EchoController.java
@@ -9,6 +9,4 @@ public class EchoController {
     @ResponseBody
     @RequestMapping(value="/{str}", method = RequestMethod.GET)
     public String echo(@PathVariable("str") String str) { return str; }
-
-
 }
